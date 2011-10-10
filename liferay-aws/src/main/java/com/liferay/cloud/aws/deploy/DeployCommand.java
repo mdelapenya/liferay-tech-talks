aad16c1c019ef9a89d90d4c22e3689c1ec80cada
Miguel Pastor <miguel.pastor@liferay.com>
2011-10-10 17:06:39 +0200

    Basic deploy command

---
+++ liferay-aws/src/main/java/com/liferay/cloud/aws/deploy/DeployCommand.java
@@ -0,0 +1,13 @@
+package com.liferay.cloud.aws.deploy;
+
+import com.liferay.cloud.aws.console.Command;
+
+public class DeployCommand implements Command {
+	
+	@Override
+	public void execute() {
+	}
+
+	private String accessKeyId;
+	private String secretKey;
+}
