f3fcb7b2df8639c0215bff54c73440ea2d196420
Miguel Pastor <miguel.pastor@liferay.com>
2011-10-26 23:12:01 +0200

    AWS Configuration

---
+++ liferay-aws/src/main/resources/com/liferay/cloud/setup/AWSToolsConfiguration.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export AWS_HOME=/opt/aws
+export EC2_HOME=${AWS_HOME}/ec2-api-tools-1.4.4.2
+export RDS_HOME=${AWS_HOME}/RDSCli-1.4.007
+export EC2_KEY_DIR=/home/migue/.ec2
+export EC2_PRIVATE_KEY=${EC2_KEY_DIR}/you-private-key.pem
+export EC2_CERT=${EC2_KEY_DIR}/your-private-cert.pem
